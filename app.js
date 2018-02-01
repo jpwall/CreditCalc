0e8432456f27022a3af047b98e418e46848f32e0
Jesse Walling <jessephilipwalling@gmail.com>
2018-02-01 11:17:06 -0800

    remove everything to start with new framework

--- app.js
+++
@@ -1,6 +0,0 @@
-var express = require('express');
-var app = express();
-
-//app.get('/data', (req, res) => res.sendFile('data.json'));
-app.use(express.static('.', {extensions: 'json'}))
-app.listen(4201, () => console.log('Listening at 4201'));
