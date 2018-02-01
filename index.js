cec941dd1b8400e25f210eb7eea748609e549b95
Jesse Walling <jessephilipwalling@gmail.com>
2018-02-01 11:18:55 -0800

    add back the old html and css, include express and node as the only required server side software

---
+++ index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+var router = express.Router();
+
+app.use(express.static('src', {extensions: 'json'}));
+
+router.get('/', function(req, res, next) {
+  res.send('src/index.html');
+});
+
+router.get('/data', function(req, res, netxt) {
+  res.sendFile('src/data.json');
+});
+
+app.listen(4200, () => console.log('Listening on port 4200'));
