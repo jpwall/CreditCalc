e1c84690a1c9ed2e70fd1ed31f421beade8558cc
Jesse Walling <jessephilipwalling@gmail.com>
2018-03-01 11:53:33 -0800

    add more endpoints to be integrated with software

--- index.js
+++ index.js
@@ -20,10 +20,53 @@ for (i = 0; i < data.length; i++) {
   courseNames.push(data[i].coursename);
 }
 
+var courseLengths = [];
+for (i = 0; i < data.length; i++) {
+  courseLengths.push(data[i].courselength);
+}
+
+var courseTypes = [];
+for (i = 0; i < data.length; i++) {
+  courseTypes.push(data[i].coursetype);
+}
+
+var courseCredits = [];
+for (i = 0; i < data.length; i++) {
+  courseCredits.push(data[i].credit);
+}
+/*
+function onlyUnique(value, index, self) { 
+    return self.indexOf(value) === index;
+}
+var separatedCredits = [];
+for (i = 0; i < courseCredits.length; i++) {
+  var itemSplit = [];
+  itemSplit = courseCredits[i].split(', ');
+  for (i = 0; i < itemSplit.length; i++) {
+    separatedCredits.push(itemSplit[i]);
+  }
+}*/
+//var allCredits = [];
+//allCredits = separatedCredits.filter(onlyUnique);
+
 app.get('/courseNames', function(req, res) {
   res.send(courseNames);
-  //res.sendJson(data.findAll);
 });
 
+app.get('/courseLengths', function(req, res) {
+  res.send(courseLengths);
+});
 
+app.get('/courseTypes', function(req, res) {
+  res.send(courseTypes);
+});
+
+app.get('/courseCredits', function(req, res) {
+  res.send(courseCredits);
+});
+/*
+app.get('/allCredits', function(req, res) {
+  res.send(separatedCredits);
+});
+*/
 app.listen(100, () => console.log('Listening on port 100'));
