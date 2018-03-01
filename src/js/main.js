901ff33ab01c8cf28b5df0971d48bbb6c1eb0157
Jesse Walling <jessephilipwalling@gmail.com>
2018-03-01 10:43:07 -0800

    update repo

--- src/js/main.js
+++ src/js/main.js
@@ -5,6 +5,18 @@ $.ajax({
   var courseNamesRaw = data + "";
   courseNames = courseNamesRaw.split(",");
   for (var i=0; i<courseNames.length; i++) {
-    $(".selectpicker").append("<option>" + courseNames[i] + "</option");
+    $(".selectpicker").append("<option>" + courseNames[i] + "</option>");
   };
 });
+/*
+var courseNamesRaw = $.ajax({
+  url: "CourseNames"
+}).done(function (data) {
+  return data;
+});
+//var courseNamesRaw = jQuery.get("courseNames") + "";
+var courseNames = courseNamesRaw.split(",");
+//console.log(courseNamesRaw);
+for (var i = 0; i<courseNames.length; i++) {
+  $(".selectpicker").append("<option>" + courseNames + "</option>");
+};*/
